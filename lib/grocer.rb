c82a50cdd90a6037b8e4086b552bf89222d029f3
Anastasia Orlova <metkusblackus@gmail.com>
2020-05-08 13:46:11 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,17 @@
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
-  # Consult README for inputs and outputs
+  # Consult README for inputs and outputs.
+  #It should then iterate over the collection and check the value of :item in each nested hash.
+  #If a match is found, the method should return that entire hash. For instance, if we ran:
+
+  collection.each do |hash, element|
+    if name == element[:item]
+      return hash
+    elsif name == nil
+      return nil
+    end
+  end
 
 end
 
@@ -12,6 +22,3 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
 end
-
-
-  
